0146368f09b7153c17a948690933b0011c069afc
Max Woollons <maxwoollons@outlook.com>
2023-10-29 13:33:47 +1000

    Update Data and entire app

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 
+
 let loader = document.getElementById("loading-item");
+
+
+let api_key = "37bf2c1109e88abd6360835f1bae2db5"
 
 aircraft_data = {
     B736: {
         name: "Boeing 737-600",
-        maxPassengers: 149,
-        averageSpeed: 969.318, // kmph
+        averageSpeed: 850, // kmph
+        fuelPerKm: 3.16, //kg
+    },
+    B737: {
+        name: "Boeing 737-700",
+        averageSpeed: 876, // kmph
+        fuelPerKm: 3.21, //kg
+    },
+    B738: {
+        name: "Boeing 737-800",
+        averageSpeed: 842, // kmph
+        fuelPerKm: 3.58, //kg
+    },
+    A320n: {
+        name: "Airbus A320neo",
+        averageSpeed: 840, // kmph
+        fuelPerKm: 2.79, //kg
+    },
+    A321n: {
+        name: "Airbus A321neo",
+        averageSpeed: 833, // kmph
+        fuelPerKm: 3.30, //kg
     },
     C172: {
         name: "Cessna 172",
-        maxPassengers: 4,
         averageSpeed: 226, //kmph
+        fuelPerKm: 0.03, //kg
+
 },
 C152: {
     name: "Cessna 152",
-    maxPassengers: 2,
     averageSpeed: 198, //kmph
-}
+    fuelPerKm: 0.03, //kg
+
+
+},
+
 }
 
 
@@ -74,6 +102,42 @@ function calculateDistance(event){
             results_div.style.display = "block";
 
 
+            // Weather data
+            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat1 + '&lon=' + lon1 + '&appid=' + api_key)
+            .then(response => response.json())
+            .then(data => {
+                let temp = data.main.temp - 273.15;
+                if (document.getElementById("corf").value == "f") {
+                    temp = (temp * 9/5) + 32;
+                    var symbol = "°F"
+                } else {
+                    var symbol = "°C"
+                }
+                
+                weatherResult = document.getElementById("weather1");
+                weatherResult.innerHTML = data.weather[0].description + "<br/>" + (temp).toFixed(2) + symbol + "<br/>" + (data.wind.speed*1.944).toFixed(2) + "knots/s @ " + data.wind.deg + "°";
+            })
+            .catch(error => console.log(error));
+
+            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat2 + '&lon=' + lon2 + '&appid=' + api_key)
+            .then(response => response.json())
+            .then(data => {
+                temp = data.main.temp - 273.15;
+                if (document.getElementById("corf").value == "f") {
+                    temp = (temp * 9/5) + 32;
+                    var symbol = "°F"
+                } else {
+                    var symbol = "°C"
+                }
+
+                weatherResult = document.getElementById("weather2");
+                weatherResult.innerHTML = data.weather[0].description + "<br/>" + (temp).toFixed(2) + symbol +"<br/>" + (data.wind.speed*1.944).toFixed(2) + "knots/s @ " + data.wind.deg + "°";
+            })
+            .catch(error => console.log(error));
+
+
+
+
             // loader.style.display = "none";
             saresult = document.getElementById("start-airport");
             saresult.innerHTML = start + " - " + airportData[0].name + "<br/>" + airportData[0].type + " - " +  airportData[0].continent;
@@ -83,6 +147,15 @@ function calculateDistance(event){
             distanceResult.innerHTML = "Distance: " + d.toFixed(2) + " " + unit_symbol;
             timeResult = document.getElementById("flight-time");
             timeResult.innerHTML = "Time: " + (time / 0.60).toFixed(2) + " hours";
+            fuelResult = document.getElementById("fuel");
+            fuelAmount = (d_km * aircraft_data[plane].fuelPerKm).toFixed(2);
+            if (document.getElementById("lborkg").value == "lb") {
+                fuelAmount = fuelAmount * 2.20462;
+                fuelResult.innerHTML = "Fuel: " + (fuelAmount).toFixed(2) + " lb";
+            } else {
+                fuelResult.innerHTML = "Fuel: " + fuelAmount + " kg <br/>" + (fuelAmount / 1000).toFixed(2) + " Tons";
+
+            }
             
 
             
